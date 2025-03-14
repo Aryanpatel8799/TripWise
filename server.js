aef6014b0ce4aff64ae2684165608498d4b3502d
Aryan Patel <amp8799@gmail.com>
2025-03-14 09:40:26 +0530

    Server Created

---
+++ server.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.json({ message: 'Welcome to TripWise API' });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
